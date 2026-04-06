a622ad6da907510788c479bed6e39714ed8368a4
Theophilos Ioakeimidis <ramones.teo@gmail.com>
2026-04-06 19:42:03 +0300

    Revert KTS format

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,27 +13,28 @@ pluginManagement {
     }
 }
 plugins {
-    id("org.gradle.experimental.android-ecosystem").version("0.1.57")
+//    id("org.gradle.experimental.android-ecosystem").version("0.1.57")
     id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
-defaults {
-    androidApplication {
-        jdkVersion = 17
-        minSdk = 21
-        targetSdk = 34
-        compileSdk = 36
-
-        lint {
-            enabled = true
-            }
-        }
-}
+//defaults {
+//    androidApplication {
+//        jdkVersion = 17
+//        minSdk = 21
+//        targetSdk = 34
+//        compileSdk = 36
+//
+//        lint {
+//            enabled = true
+//            }
+//        }
+//}
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
-    repositoriesMode = FAIL_ON_PROJECT_REPOS
+//    repositoriesMode = FAIL_ON_PROJECT_REPOS
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
